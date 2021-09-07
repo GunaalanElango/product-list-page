df68304c779e9e1a93cd83fd7000a589d21e5d5c
GunaalanElango <elangogunaalan@gmail.com>
2021-09-07 17:10:36 +0530

    displaying price in indian currency format

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,18 @@ import React from "react";
 import classes from "./Product.module.css";
 
 const Product = ({ name, price, imageURL }) => {
+  let formattedCurrency = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+  }).format(price);
+
   return (
     <div className={classes.Product}>
       <div className={classes.ImageContainer}>
         <img alt="Product" src={imageURL} />
       </div>
       <p className={classes.ProductName}>{name}</p>
-      <p className={classes.ProductPrice}>&#8377; {price}</p>
+      <p className={classes.ProductPrice}>{formattedCurrency}</p>
     </div>
   );
 };
